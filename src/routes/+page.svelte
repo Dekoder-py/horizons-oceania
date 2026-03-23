c7fd6175532199c2bc7a03c765d83114e41aef29
Dekoder-py <kyle@codingcorner.dev>
2026-03-24 09:52:15 +1300

    add hack club flag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,14 @@
 	<title>{eventName}</title>
 </svelte:head>
 
+<a href="https://hackclub.com/"
+	><img
+		style="position: absolute; top: 0; left: 10px; border: 0; width: 256px; z-index: 999;"
+		src="https://assets.hackclub.com/flag-orpheus-top.svg"
+		alt="Hack Club"
+	/></a
+>
+
 <div id="hero">
 	<div id="title">
 		<h2>Hack Club</h2>
