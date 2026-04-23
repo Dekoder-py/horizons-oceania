ce8248f37e19efebe8c3155372516e70944e8da3
Dekoder-py <kyle@codingcorner.dev>
2026-04-23 16:58:11 +1200

    update event details page

--- src/pages/EventDetails.tsx
+++ src/pages/EventDetails.tsx
@@ -17,36 +17,36 @@ export default function EventDetails() {
 
         <h3>What to Expect</h3>
         <p>
-          Join 100+ talented teenagers from around the world for an incredible
-          hackathon experience! Horizons Crux is designed to inspire, educate,
-          and celebrate the creative projects you build.
+          Participants are expected to arrive before 1pm On July 10th and will
+          leave at 1pm on July 12th. Accommodation will be provided at the
+          venue.
         </p>
-        <ul>
-          <li>Build amazing projects with your teammates</li>
-          <li>Learn from mentors and industry professionals</li>
-          <li>Meet teenagers from all around the world</li>
-          <li>Experience Sydney's vibrant tech community</li>
-          <li>Showcase your projects to the community</li>
-          <li>Win prizes and recognition</li>
-        </ul>
-
-        <h3>Schedule</h3>
+        <p>The venue is currently TBA (to be announced).</p>
         <p>
-          <b>July 10:</b> Welcome & Project Planning
-          <br />
-          <b>July 11:</b> Building Day (Full Day)
-          <br />
-          <b>July 12:</b> Final Presentations & Celebration
+          Food will be provided for the duration of the event, and there will be
+          accommodation provided for all dietary restrictions.
         </p>
 
-        <h3>What's Provided</h3>
-        <ul>
-          <li>✨ All meals and snacks (breakfast, lunch, dinner, snacks)</li>
-          <li>🛏️ Accommodation for out-of-town participants</li>
-          <li>✈️ Flight stipends to cover travel costs</li>
-          <li>🎁 Exclusive Horizons Crux swag</li>
-          <li>🏆 Awards and recognition</li>
-        </ul>
+        <p>
+          Participants will sleep at the hackathon venue. We recommend you bring
+          a sleeping bag!
+        </p>
+
+        <p>
+          There will be background-checked staff on site for the entire event.
+        </p>
+
+        <p>
+          Closer to the event, we'll be hosting pre-event calls to update you on
+          logistics!
+        </p>
+
+        <h3>Are parents allowed to come?</h3>
+        <p>
+          Parents will <b>not</b> be allowed to stay in the venue, and will not
+          be reimbursed for their travel or stay. They may check the venue to
+          make sure it is safe under the supervision of staff if necessary.
+        </p>
 
         <h3>Questions?</h3>
         <p>
